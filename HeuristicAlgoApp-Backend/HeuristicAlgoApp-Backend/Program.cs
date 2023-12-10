fb85daf20c47f2066d31d1b63cc1a3ef659e9518
Gesiek <70916807+Gesiek@users.noreply.github.com>
2023-12-10 14:24:56 +0100

    Dodanie polityki CORS
    żeby działał nasz front z backiem lokalnie; takie coś ala wyjątek do zapory

--- HeuristicAlgoApp-Backend/HeuristicAlgoApp-Backend/Program.cs
+++ HeuristicAlgoApp-Backend/HeuristicAlgoApp-Backend/Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// dodanie serwisu polityki CORS
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        // zaufaj temu adresowi niezaleznie od naglowka/metody
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
