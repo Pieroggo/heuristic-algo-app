8cdbc0cff265bcb09be9d6aeb75d4b3d87d617d0
Gesiek <70916807+Gesiek@users.noreply.github.com>
2023-12-10 14:25:58 +0100

    Zbiera dane z naszego backendu
    używa naszego api; na funkcje i algorytmy

--- HeuristicAlgoApp-Frontend/system-frontend/src/App.js
+++ HeuristicAlgoApp-Frontend/system-frontend/src/App.js
@@ -12,9 +12,13 @@ function App() {
 
   // dane z bazy
   useEffect(() => {
-    axios.get(appStore.apiPath + '/users')
+    axios.get(appStore.apiPath + '/Algorithm/GetAll')
       .then(response => {
         appStore.algorithms = response.data
+      })
+    axios.get(appStore.apiPath + '/FitnessFunction')
+      .then(response => {
+        appStore.functions = response.data
       })
   }, [])
 
@@ -23,19 +27,44 @@ function App() {
     <div className="App">
 
       <div>
+        <p>Algorytmy: </p>
         <table className="bp4-html-table .modifier">
           <thead>
             <tr>
-              <th>Nazme</th>
-              <th>Email</th>
+              <th>l.p.</th>
+              <th>nazwa</th>
             </tr>
           </thead>
-          {appStore.algorithms.map(algo => (
-            <tr key={algo.id}>
-              <td>{algo.name}</td>
-              <td>{algo.email}</td>
+          <tbody>
+            {appStore.algorithms.map(algo => (
+              <tr key={algo.id}>
+                <td>{}</td>
+                <td>{algo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <br/>
+      
+      <div>
+        <p>Funkcje: </p>
+        <table className="bp4-html-table .modifier">
+          <thead>
+            <tr>
+              <th>l.p.</th>
+              <th>nazwa</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {appStore.functions.map(func => (
+              <tr key={func.id}>
+                <td>{}</td>
+                <td>{func}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
 
--- HeuristicAlgoApp-Frontend/system-frontend/src/AppStore.js
+++ HeuristicAlgoApp-Frontend/system-frontend/src/AppStore.js
@@ -3,9 +3,11 @@ import axios from "axios";
 
 export default class AppStore {
 
-    //apiPath = "https://localhost:7188/";
-    apiPath = 'https://jsonplaceholder.typicode.com/';
+    //apiPath = 'https://jsonplaceholder.typicode.com/';
+    apiPath = 'https://localhost:7071/api';
+
     algorithms = [];
+    functions = [];
 
     constructor() {
         makeAutoObservable(this);
