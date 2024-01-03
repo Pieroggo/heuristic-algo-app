5f49b124d17949394b973eac3ba68d7ca539c95e
Gesiek <70916807+Gesiek@users.noreply.github.com>
2024-01-03 13:05:19 +0100

    Uaktualnienia na froncie
    wybór funkcji i algorytmów i ich parametrów, prawie gotowe; przygotowanie pod Single i Multi taski

--- HeuristicAlgoApp-Frontend/system-frontend/src/AppStore.js
+++ HeuristicAlgoApp-Frontend/system-frontend/src/AppStore.js
@@ -1,4 +1,4 @@
-import { makeAutoObservable } from "mobx";
+import { action, makeAutoObservable, runInAction } from "mobx";
 import axios from "axios";
 
 export default class AppStore {
@@ -14,6 +14,11 @@ export default class AppStore {
     functions = [];
     file = "";
 
+    przerwanoSingleTask = false;
+    przerwanoMultiTask = false;
+
+    // TO DO break functionality
+
     setAlgorithms = (data) => {
         this.algorithms = data;
     }
@@ -23,14 +28,14 @@ export default class AppStore {
     }
 
     handleFileChange = (event) => {
-        
+
         console.log("> Zmieniam plik");
-        
+
         if (event.target.files && event.target.files.length > 0) {
             const selectedFile = event.target.files[0];
             this.file = selectedFile;
         }
-        
+
         console.log(this.file);
     };
 
@@ -51,10 +56,177 @@ export default class AppStore {
         else {
             console.log("Brak pliku do przesłania")
         }
-        
+
         this.file = ""
         console.log("Czyszcze input")
         document.getElementById("fileUpload").value = ""
+    }
+
+    // FUNCTION get func of id
+
+    getFunctionById = (id) => {
+        let f = this.functions.filter((f) => {return f.id == id})[0]
+        console.log("> Wybieram funkcje: ", f.id, " : ", f.name)
+        return f
+    }
+
+
+    // PARAMETERS changing
+
+    // setFuntionDimension = (id, dim) => {
+    //     this.getFunctionById(id).dimension = dim
+    // }
+
+    setFuntionDimension = (id, dim) => {
+        runInAction(() => {
+            this.getFunctionById(id).dimension = dim
+        })
+    }
+
+    handleFuncDimChange = (e, id) => {
+        
+        // zmiejszamy
+        if(this.getFunctionById(id).dimension > e.target.value){
+            this.getFunctionById(id).lowerBoundaries.pop()
+        }
+        if(this.getFunctionById(id).dimension > e.target.value){
+            this.getFunctionById(id).upperBoundaries.pop()
+        }
+        
+        // zwiekszamy
+        if(this.getFunctionById(id).dimension < e.target.value){
+            this.getFunctionById(id).lowerBoundaries.push(0)
+        }
+        if(this.getFunctionById(id).dimension < e.target.value){
+            this.getFunctionById(id).upperBoundaries.push(0)
+        }
+
+        this.setFuntionDimension(id, e.target.value)
+    }
+    
+    handleFuncLowBoundChange = (e, dim, id) => {
+        id = id-1
+        this.functions[id].lowerBoundaries[dim] = e.target.value
+    }
+
+    // TASKs
+
+    runSinlgeTask = async () => {
+
+        console.log("> Uruchamiam SINGLE TASK")
+
+        let algoID = document.querySelector('input[name="algo-one"]:checked')
+        if (algoID) {
+            console.log("Wybrany algorytm: ", algoID.value)
+        }
+        else {
+            console.log("Nie wybrano algorytmu")
+        }
+
+        let funcIDs = []
+        let checkboxes = document.querySelectorAll('input[name="func-many"]:checked')
+        if (checkboxes.length > 0) {
+            checkboxes.forEach((checkbox) => {
+                funcIDs.push(checkbox.value)
+            })
+            console.log("Wybrane funkcje: ", funcIDs)
+        }
+        else {
+            console.log("Nie wybrano funkcjów")
+        }
+
+        console.log("Czyszcze inputy")
+        if (algoID) { algoID.checked = false }
+        if (checkboxes.length > 0) {
+            checkboxes.forEach((checkbox) => {
+                checkbox.checked = false
+            })
+        }
+
+    }
+
+    algoIDs = []
+
+    handleAlgoIDsChange = () => {
+        this.algoIDs = []
+        console.log("> algoIDs change")
+        let checkboxes = document.querySelectorAll('input[name="algo-many"]:checked')
+        if (checkboxes.length > 0) {
+            checkboxes.forEach((checkbox) => {
+                this.algoIDs.push(checkbox.value)
+            })
+        }
+    }
+    
+    funcID = null
+    
+    handleFuncIDChange = () => {
+        console.log("> funcID change")
+        document.querySelector('input[name="func-one"]:checked')
+            ?
+            this.funcID = document.querySelector('input[name="func-one"]:checked').value
+            :
+            this.funcID = null
+    }
+
+    runMultiTask = async () => {
+
+        console.log("> Uruchamiam MULTI TASK")
+
+        if (this.algoIDs.length > 0) {
+            console.log("Wybrane algosy: ")
+            this.algoIDs.forEach((id) => {
+                console.log(id)
+            })
+        }
+        else {
+            console.log("Nie wybrano algorytmów")
+        }
+
+        if (this.funcID) {
+            console.log("Wybrana funkcja: ", this.funcID)
+        }
+        else {
+            console.log("Nie wybrano funkcji")
+        }
+
+        // let algoIDs = []
+        // let checkboxes = document.querySelectorAll('input[name="algo-many"]:checked')
+        // if (checkboxes.length > 0) {
+        //     checkboxes.forEach((checkbox) => {
+        //         algoIDs.push(checkbox.value)
+        //     })
+        //     console.log("Wybrane algosy: ", algoIDs)
+        // }
+        // else {
+        //     console.log("Nie wybrano algorytmów")
+        // }
+
+        // let funcID = document.querySelector('input[name="func-one"]:checked')
+
+        // if (funcID) {
+        //     console.log("Wybrana funckja: ", funcID.value)
+        // }
+        // else {
+        //     console.log("Nie wybrano funkcji")
+        // }
+
+
+
+
+        console.log("Czyszcze inputy")
+        let checkboxes = document.querySelectorAll('input[name="algo-many"]:checked')
+        if (checkboxes.length > 0) {
+            checkboxes.forEach((checkbox) => {
+                checkbox.checked = false
+            })
+            this.algoIDs = []
+        }
+        if (this.funcID) {
+            document.querySelector('input[name="func-one"]:checked').checked = false
+            this.funcID = null
+        }
+
     }
 
 
---
+++ HeuristicAlgoApp-Frontend/system-frontend/src/assets/Inputs.js
@@ -0,0 +1,155 @@
+import { useStore } from "../store";
+import { observer } from "mobx-react-lite";
+import BoundariesFor from "./BoundariesFor";
+import Parameters from "./Parameters";
+
+export default observer(function Inputs({ which, many }) {
+
+    const { appStore } = useStore();
+
+    // for SINGLE TASK
+
+    // one algo
+
+    if (which === "algo" && many === false) {
+        return (
+            <table className="bp4-html-table .modifier">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>nazwa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appStore.algorithms.map(algo => (
+                        <tr key={algo.id}>
+                            <td>{algo.id}</td>
+                            <td>
+                                <label htmlFor={algo.name + "_one"}>
+                                    {algo.name}
+                                </label>
+                            </td>
+                            <td>
+                                <input type="radio" id={algo.name + "_one"} value={algo.id} name="algo-one"></input>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+
+    // many functions
+
+    if (which === "func" && many === true) {
+        return (
+            <table className="bp4-html-table .modifier">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>nazwa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appStore.functions.map(func => (
+                        <tr key={func.id}>
+                            <td>{func.id}</td>
+                            <td>
+                                <label htmlFor={func.name + "_many"}>
+                                    {func.name}
+                                </label>
+                            </td>
+                            <td>
+                                <input type="checkbox" id={func.name + "_many"} value={func.id} name="func-many"></input>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+
+    // for MULTI TASK
+
+    // many algos
+
+    if (which === "algo" && many === true) {
+        return (
+            <table className="bp4-html-table .modifier">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>nazwa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appStore.algorithms.map(algo => (
+                        <tr key={algo.id}>
+                            <td>{algo.id}</td>
+                            <td>
+                                <label htmlFor={algo.name + "_many"}>
+                                    {algo.name}
+                                </label>
+                            </td>
+                            <td>
+                                <input type="checkbox" id={algo.name + "_many"} value={algo.id} name="algo-many" onChange={() => appStore.handleAlgoIDsChange()}></input>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+
+    // one function
+
+    if (which === "func" && many === false) {
+        return (
+            <table className="bp4-html-table .modifier">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>nazwa</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appStore.functions.map(func => (
+                        <tr key={func.id}>
+                            <td>{func.id}</td>
+                            <td>
+                                <label htmlFor={func.name + "_one"}>
+                                    {func.name}
+                                </label>
+                                {appStore.funcID == func.id &&
+                                    // może tak?
+                                    // <Parameters id={func.id}/>
+                                    <div>
+                                        <br />
+                                        <label htmlFor="dim_f_one">wymiary: </label>
+                                        <input type="number" id="dim_f_one" value={func.dimension} maxLength={1} className="smallNumInput" onChange={(e) => appStore.handleFuncDimChange(e, func.id)}></input>
+
+                                        <p>&lt; dolna granica &#59; górna granica &gt;</p>
+                                        <BoundariesFor id={func.id}/>
+
+                                        {/* <br />
+                                        <label>dolna granica:
+                                            <input type="number" id={func.lowerBoundaries[0] + "_one"} value={func.lowerBoundaries[0]} className="mediumNumInput" onChange={(e) => appStore.handleFuncLowBoundChange(e, 1, func.id)}></input>
+                                        </label> */}
+                                    </div>
+                                }
+                            </td>
+                            <td>
+                                <input type="radio" id={func.name + "_one"} value={func.id} name="func-one" onChange={() => { appStore.handleFuncIDChange() }}></input>
+                            </td>
+                        </tr>
+                        // TO DO wyświetlenie paramsów dla wybranych
+                    ))}
+                </tbody>
+            </table>
+        )
+    }
+
+
+
+
+})
